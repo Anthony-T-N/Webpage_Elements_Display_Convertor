4b6d363660b7eb96633487f3ecd354412d08fff3
Anthony-T-N <53560209+Anthony-T-N@users.noreply.github.com>
2022-09-11 18:23:27 +1000

    Update Webpage_Elements_Display_Convertor.cpp

--- Webpage_Elements_Display_Convertor.cpp
+++ Webpage_Elements_Display_Convertor.cpp
@@ -6,18 +6,21 @@
 
 int generate_HTML_file(std::vector<std::string> test)
 {
+    std::ifstream input_file;
+    input_file.open("elements_webpage_template.html");
     std::ofstream output_file;
-    output_file.open("report.html");
-    std::vector<std::string> temp_report_c = {};
-    // Output file stream allows you to write contents to a file.
-    std::ofstream output_file;
-    if (std::filesystem::exists("List.csv") == false)
+    if (std::filesystem::exists("test.html") == false)
     {
         std::cout << "[!] Creating/opening List.csv;" << "\n";
         output_file.open("test.html", std::ios::app);
         std::cout << "[+] Opened List.csv successfully;" << "\n";
-        output_file << line << "\n";
-        temp_report_c.push_back(line);
+        std::string input_file_line;
+        while (std::getline(input_file, input_file_line))
+        {
+            std::cout << input_file_line << "\n";
+            output_file << input_file_line << "\n";
+        }
+        input_file.close();
         output_file.close();
         return 0;
     }
@@ -36,6 +39,7 @@ std::vector<std::string> read_file()
         std::cout << input_file_line << "\n";
         input_file_line_vec.push_back(input_file_line);
     }
+    input_file.close();
     return input_file_line_vec;
 }
 
